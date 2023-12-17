97d2aa7872f23acb49f26137be64e15b7c32e4e6
pempek18 <pempek183@gmail.com>
2023-12-17 17:16:56 +0100

    First complete version of class

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Spherical_coordinate_system
 Python class Spherical provides possibility to calculate cartesian points to Spherical and plot it. 
+
+![Alt text](./data//Figure_1.png?raw=true "3D Printed Spherical arrow")
+
+![Alt text](./data//Figure_2.png?raw=true "Printed Spherical arrow")
+
+examples of class usage : 
+    x = float(input("Type x: "))
+    y = float(input("type y: ")) 
+    z = float(input("type z: "))
+    s = Spherical(print=True)
+    r       = s.r(x,y,z)
+    theta   = s.theta(x=x,y=y,z=z)
+    phi     = s.phi(x=x,y=y)
+    print(f"x={x} y={y}, z={z} r={format(r, '.2f')}, theta={format(theta,'.2f')}, phi={format(phi, '.2f')}")
+    s.plot_data()
+
--- Spherical.py
+++ Spherical.py
@@ -4,14 +4,14 @@ from typing import Literal
 class Spherical :
     def __init__(self, print=False) :
         self.is_printing = print 
-    var_x = 0
-    var_y = 0
-    var_z = 0
-    var_r = 0
-    var_theta = 0
-    var_phi = 0 
+    __var_x : float = 0
+    __var_y : float = 0
+    __var_z : float = 0
+    __var_r : float = 0
+    __var_theta : float = 0
+    __var_phi   : float = 0 
     
-    def _quadrants(self, func:Literal['sin', 'cos', 'tan', 'ctg'], x : float, y : float, z : float = 0) :
+    def _quadrants(self, func:Literal['sin', 'cos', 'tan', 'ctg'], x : float, y : float ) :
         if x==0 and y==0 :
             raise ValueError("Wrong arguments, please type at least arguments x,y,")
         if func not in ['sin', 'cos', 'tan', 'ctg']:
@@ -44,26 +44,30 @@ class Spherical :
             value = quadrants_values[3,functions[func]]      
             _print_Value(value=value, quadrant=4, x=x, y=y)               
             return value
-    def r(self,x,y, z=0) :
-        self.var_x = x 
-        self.var_y = y 
-        self.var_z = z      
-        self.var_r = np.sqrt(pow(x,2) + pow(y,2) + pow(z,2))
-        return self.var_r
-    def theta(self,x,y,z = 0) :
-        self.var_x = x 
-        self.var_y = y 
-        self.var_z = z 
-        if z != 0 :
-            self.var_theta = np.tanh((np.sqrt(pow(x,2) + pow(y,2)) / z )) * self._quadrants('tan', x, y)
         else :
-            self.var_theta = 1
-        return self.var_theta
-    def phi(self,x,y):
-        self.var_x = x 
-        self.var_y = y 
+            return 1 
+    def r(self,x : float,y : float, z : float=0) :
+        self.__var_x = x 
+        self.__var_y = y 
+        self.__var_z = z      
+        self.__var_r = np.sqrt(pow(x,2) + pow(y,2) + pow(z,2))
+        return self.__var_r
+    def theta(self,x : float,y : float,z : float = 0) :
+        self.__var_x = x 
+        self.__var_y = y 
+        self.__var_z = z 
+        if z != 0 :
+            self.__var_theta = np.arctan((np.sqrt(pow(x,2) + pow(y,2)) / z )) 
+        elif x * y >= 0 :
+            self.__var_theta = np.pi / 2
+        elif x * y < 0 :
+            self.__var_theta = -np.pi / 2
+        return self.__var_theta
+    def phi(self,x : float,y : float):
+        self.__var_x = x 
+        self.__var_y = y 
 
-        if x != 0 :
+        if x != 0 and y != 0:
             phi_quater = 0 
             if x > 0 and y > 0 :
                 phi_quater = 0 ; 
@@ -73,42 +77,46 @@ class Spherical :
                 phi_quater = np.pi 
             if x > 0 and y < 0 :
                 phi_quater = np.pi * 3  / 2                             
-            self.var_phi = (np.tanh((y / x )) * self._quadrants('tan',x=x,y=y)) + phi_quater
+            self.__var_phi = (np.arctan((y / x )) * self._quadrants('tan',x=x,y=y)) + phi_quater
         else:
-            if y > 0 :
-                self.var_phi = np.tanh(np.pi/2)
-            else :
-                self.var_phi = np.tanh(np.pi * 3 / 2)
-        return self.var_phi
-    def x (self, r, theta, phi) :
-        self.var_r      = r 
-        self.var_theta  = theta 
-        self.var_phi    = phi 
-        x = r * np.sin(theta) * np.cos(phi)
+            if x==0 and y > 0 :
+                self.__var_phi = np.pi/2
+            elif x ==0 and y < 0 :
+                self.__var_phi = np.pi * 3 / 2
+            elif x > 0 and y == 0 :
+                self.__var_phi = 0
+            elif x < 0 and y==0 :
+                self.__var_phi = np.pi
+        return self.__var_phi
+    def x (self, r : float, theta : float, phi : float) :
+        self.__var_r      = r 
+        self.__var_theta  = theta 
+        self.__var_phi    = phi 
+        x = r * np.sin(theta) * np.cos(phi) * (1 if (theta > 0) else -1)
         return x 
-    def y (self,r, theta, phi):
-        self.var_r      = r 
-        self.var_theta  = theta 
-        self.var_phi    = phi      
-        y = r * np.sin(theta) * np.sin(phi) 
+    def y (self,r : float, theta : float, phi : float):
+        self.__var_r      = r 
+        self.__var_theta  = theta 
+        self.__var_phi    = phi      
+        y = r * np.sin(theta) * np.sin(phi) * (1 if (theta > 0) else -1)
         return y
-    def z (self,r, theta):
-        self.var_r      = r 
-        self.var_theta  = theta 
-        z = r * np.cos(theta)
+    def z (self,r : float, theta : float):
+        self.__var_r      = r 
+        self.__var_theta  = theta 
+        z = r * np.cos(theta) * (1 if (theta >= 0) else -1)
         return z 
     def plot_data(self) :
-        if self.var_z == 0 :
-            # Setting the range for x and y axis
-            # x_lines = np.linspace(-10, 10, 400)
-            # y_lines = np.linspace(-10, 10, 400)
-
+        #ploting data count againg to double check the calculations
+        x_to_plot = self.x(self.__var_r, self.__var_theta, self.__var_phi)
+        y_to_plot = self.y(self.__var_r, self.__var_theta, self.__var_phi)
+        z_to_plot = self.z(self.__var_r, self.__var_theta)
+        title = f"""
+        setpoint    : x={format(self.__var_x, '.2f')} y={format(self.__var_y, '.2f')}, z={format(self.__var_z, '.2f')}
+        spherical   : r={format(self.__var_r, '.2f')}, theta={format(self.__var_theta,'.2f')}, phi={format(self.__var_phi, '.2f')}
+        recalulated: x={format(x_to_plot, '.2f')}, y={format(y_to_plot, '.2f')}, z={format(z_to_plot, '.2f')} """
+        if self.__var_z == 0 :
             # Creating the figure and axis
             fig, ax = plt.subplots()
-
-            # Setting the limits for x and y axis
-            # ax.set_xlim([-10, 10])
-            # ax.set_ylim([-10, 10])
 
             # Drawing axis lines
             ax.axhline(y=0, color='k')
@@ -121,37 +129,34 @@ class Spherical :
             ax.set_xlabel('X-axis')
             ax.set_ylabel('Y-axis')
 
-            # Title of the graph
-            ax.set_title(f"x={self.var_x} y={self.var_y}, z={self.var_z} r={format(self.var_r, '.2f')}, theta={format(self.var_theta,'.2f')}, phi={format(self.var_phi, '.2f')}")
-            ax.arrow(0, 0, self.x(self.var_r, self.var_theta, 
-            self.var_phi) , self.y(self.var_r, self.var_theta, self.var_phi), head_width=0.2, head_length=0.2, fc='blue', ec='blue')
-            ax.scatter(x=self.var_x,y=self.var_y, color='red')
+            ax.arrow(x=0, y=0, 
+                     dx=x_to_plot, 
+                     dy=y_to_plot, 
+                     head_width=0.1, 
+                     head_length=0.1, 
+                     fc='blue', 
+                     ec='blue',
+                     label='recarculated x y')
+            ax.scatter(x=self.__var_x,y=self.__var_y, color='red', label='setpoint')
         else :
             # Creating the 3D figure
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
 
-            ax.quiver(0, 0, 0, self.x(self.var_r, self.var_theta, self.var_phi), self.y(self.var_r, self.var_theta, self.var_phi), self.z(self.var_r, self.var_theta), color='red')
-            ax.scatter(x=self.var_x,y=self.var_y, z=self.var_z)
-            # Setting the limits for x, y, and z axis
-            # ax.set_xlim([-4, 4])
-            # ax.set_ylim([-4, 4])
-            # ax.set_zlim([-4, 4])
+            ax.quiver(0, 0, 0, x_to_plot, y_to_plot, z_to_plot, color='red', label='recalulated x y')
+            ax.scatter(xs=self.__var_x,ys=self.__var_y, zs=self.__var_z, label='setpoint')
 
             # Labeling the axes
             ax.set_xlabel('X-axis')
             ax.set_ylabel('Y-axis')
             ax.set_zlabel('Z-axis')
-
-            # Title of the graph
-            ax.set_title(f"x={self.var_x} y={self.var_y}, z={self.var_z} r={format(self.var_r, '.2f')}, theta={format(self.var_theta,'.2f')}, phi={format(self.var_phi, '.2f')}")
-
+        plt.title(title, fontsize=11, pad=-20, loc='left')
         plt.show()    
 if __name__ == '__main__':
     x = float(input("Type x: "))
     y = float(input("type y: ")) 
     z = float(input("type z: "))
-    s = Spherical(print=False)
+    s = Spherical(print=True)
     r       = s.r(x,y,z)
     theta   = s.theta(x=x,y=y,z=z)
     phi     = s.phi(x=x,y=y)
